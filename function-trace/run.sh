3e08b4382c0701a64c6f5f9b342d37b508193d0f
sunjangyo12 <sunjangyo12@gmail.com>
2021-03-05 03:07:09 -0500

    add functio trace untuk debuging

---
+++ function-trace/run.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+echo
+echo "#### untuk menjalankan pilih 3 lalu 4 ####"
+echo "1. compile"
+echo "2. clean"
+echo "3. run"
+echo "4. address to function name"
+echo
+
+read -p "[+] Please select number: " number
+
+if [ "$number" = "1" ]; then
+      gcc -finstrument-functions -g -c -o main.o main.c
+      gcc -c -o trace.o trace.c
+      gcc main.o trace.o -o main
+
+elif [ "$number" = "2" ]; then
+      rm main  main.o trace.o  trace.out
+
+elif [ "$number" = "3" ]; then
+      ./main
+
+elif [ "$number" = "4" ]; then
+      ./readtracelog.sh main trace.out
+fi
